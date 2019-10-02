f90d328f3ea939f82f1e2b6e986b7e89bac301b9
Tom Allen <tswallen@c02yt1lelvch.localdomain>
2019-10-02 14:17:20 -0700

    Possible structural change

--- lab-01/begin/server.js
+++ lab-01/begin/server.js
@@ -1,32 +1,61 @@
-require('dotenv').config();
+// LAB 01 - Web Sign-In
+
+// IMPORTS
+
+const session = require('cookie-session');
+const { auth } = require('express-openid-connect');
 const express = require('express');
-const http = require('http');
 const morgan = require('morgan');
-
-const appUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}`;
-
 const app = express();
+const ENVIRONMENT = require('dotenv').config().parsed;
 
-app.set('view engine', 'ejs');
+// CONFIG
 
-app.use(morgan('combined'));
+app.set(
+  'view engine',
+  'ejs'
+);
+
+app.use(
+  morgan('combined'),
+  express.urlencoded(
+    {
+      extended: false
+    }
+  ),
+  session(
+    {
+      name: ENVIRONMENT.NAME,
+      secret: ENVIRONMENT.COOKIE_SECRET
+    }
+  ),
+  auth(
+    {
+      auth0Logout: true
+    }
+  )
+);
+
+// ROUTES
 
 app.get('/', (req, res) => {
-  res.render('home',  { user: req.openid && req.openid.user });
+  res.render('home', {user: req.openid && req.openid.user}
+  );
 });
 
 app.get('/expenses', (req, res) => {
-  res.render('expenses', {
-    expenses: [
+  res.render('expenses', { expenses: [
       {
         date: new Date(),
         description: 'Coffee for a Coding Dojo session.',
-        value: 42,
-      }
-    ]
+        value: 42
+      }]
   });
 });
 
-http.createServer(app).listen(process.env.PORT, () => {
-  console.log(`listening on ${appUrl}`);
-});
+// RUN
+
+app.listen(
+  ENVIRONMENT.PORT,
+  () => console.log(`${ENVIRONMENT.APP_NAME} running on port ${ENVIRONMENT.PORT}`)
+);
