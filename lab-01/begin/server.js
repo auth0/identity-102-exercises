da710fe9393e93e7fb70112d5165e540d046e12d
jesposito <jedidiah.esposito@auth0.com>
2020-10-23 15:36:57 -0700

    first commit

--- lab-01/begin/server.js
+++ lab-01/begin/server.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+cconst { auth } = require('express-openid-connect');
 
 const appUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}`;
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(morgan('combined'));
+
+app.use(auth({
+  auth0Logout: true,
+  baseURL: appUrl
+}));
 
 app.get('/', (req, res) => {
   res.render('home',  { user: req.openid && req.openid.user });
